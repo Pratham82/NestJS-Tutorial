16c9e46e8cde8a00b779be4f71d4c24f1f0e1bb0
pratham82 <mali.prathamesh82@gmail.com>
2021-02-14 00:33:00 +0530

    Created service for update and delete request

--- nestjs-intro/src/products/products.service.ts
+++ nestjs-intro/src/products/products.service.ts
@@ -20,10 +20,38 @@ export class ProductService {
   }
 
   getSingleProduct(productId: string) {
-    const product = this.products.find((product) => product.id === productId)
+    const product = this.findProduct(productId)[0]
+    return { ...product }
+  }
+
+  updateProduct(productId: string, title: string, desc: string, price: number) {
+    const [product,index ]= this.findProduct(productId)
+    const updatedProduct = { ...product }
+    if (title) {
+      updatedProduct.title = title
+    }
+    if (desc) {
+      updatedProduct.description = desc
+    }
+    if (price) {
+      updatedProduct.price = price
+    }
+    this.products[index] = updatedProduct
+  }
+
+  removeProduct(productId: string) {
+    const index = this.findProduct(productId)[1]
+    this.products.splice(index,1)
+    
+  }  
+
+  private findProduct(productId: string):[Product, number] {
+    const productIndex = this.products.findIndex((product) => product.id === productId)
+    const product = this.products[productIndex]
     if (!product) {
       throw new NotFoundException('Could not find the product with given ID')
     }
-    return { ...product }
+    return [product, productIndex]
+    
   }
 }
